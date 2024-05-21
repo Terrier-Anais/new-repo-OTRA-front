61ee6f54094bbadd992db12c7b29eb922442a2d0
Mathieu Blanc <mathieub74@gmail.com>
2024-05-21 14:40:19 +0200

    feat fetch login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,32 @@
-/*const buttonSubmit = document.querySelector('submit');
-addEventListener('mouseover', () => {    
-    buttonSubmit.style.backgroundColor = '#7E8EC8'
-});*/
+const form = document.getElementById('loginForm');
 
-// script.js
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche la soumission par défaut du formulaire
+
+    // Récupère les valeurs des champs du formulaire
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    // const confirmation = document.getElementById('confirmation').value;
+    console.log(email, password);
+
+    // Crée un objet de données pour la requête
+    const formData = {
+        email: email,
+        password: password,
+        
+    };
+    console.log(formData);
+
+    
+    axios.post('http://localhost:3000/api/login', formData)
+        .then(function(response) {
+            console.log('Succès:', response.data);
+            alert('Inscription réussie !');
+            
+        })
+        .catch(function(error) {
+            console.error('Erreur:', error);
+            alert('Erreur lors de l\'inscription. Veuillez réessayer.');
+            
+        });
+});
