2b2f76620e18d3c8e260e5d74eed414876cd25cd
Davidcialone <cialonedavid@gmail.com>
2024-05-21 11:29:37 +0200

    fetch test

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
-import {
-    // tout ce qui est pour la gestion des trips
-    } from "./trips.module.js";
-    
-    
-    
-    document.addEventListener("DOMContentLoaded", async () => {
-        
-    // function listen
-    await fetchAndDisplayTrips();
-      
+getMyTrips();
+async function getMyTrips() {
+  try {
+      // Assurez-vous que l'URL est correcte et que le serveur est en cours d'exécution
+      const httpResponse = await fetch('http://localhost:3000/api/me/trips', {
+          method: 'GET', // Méthode HTTP
+          headers: {
+              'Content-Type': 'application/json' // Type de contenu
+          }
+          // Pas de 'body' nécessaire pour une requête GET
       });
+
+      if (!httpResponse.ok) { // Vérifiez si la réponse est OK
+          console.log(httpResponse);
+          return null;
+      }
+
+      const trips = await httpResponse.json(); // Convertissez la réponse en JSON
+      console.log(trips);
+      return trips; // Renvoyez les données
+
+  } catch (error) { // Attrapez les erreurs de réseau
+      console.error(error); // Log l'erreur
+      return null; // Renvoyez null en cas d'erreur
+  }
+}
