efa1e650ef733eba730f39faec80cf3377302015
SabineLuckhee <luckhee.sabine@gmail.com>
2024-05-22 15:17:55 +0200

    Fix update modal

--- js/new-trip.js
+++ js/new-trip.js
@@ -13,7 +13,6 @@ function toggleNewTripModal() {
   modalNewTrip.classList.toggle('active');
 };
 
-// script pour l'ajout d'une nouvelle card lors de la soumission du formulaire:
 // Fonction pour créer une nouvelle carte de voyage
 function createCard(title, photo, comment, dateStart, dateEnd, duration,note) {
   // Création d'un nouvel élément de carte
@@ -54,7 +53,8 @@ function createCard(title, photo, comment, dateStart, dateEnd, duration,note) {
   `;
   return card;
 }
-// Fonction pour gérer la soumission du formulaire
+
+// Fonction pour gérer la soumission du formulaire d'ajout de voyage
 function handleFormSubmission(event) {
   event.preventDefault(); // Empêcher la soumission par défaut du formulaire
 
@@ -101,19 +101,81 @@ newTripForm.addEventListener("submit", handleFormSubmission);
 newTripForm.addEventListener("submit", toggleNewTripModal);
 newTripForm.addEventListener("submit", newTripForm.reset);
 
+
 // script for the update trip modal
 const modalUpdateTrip = document.querySelector('.modal_update-trip');
 const modalUpdateTripCloseBtn = document.querySelector('.modal_update-trip_close');
 modalUpdateTripCloseBtn.addEventListener('click', toggleUpdateTripModal);
 modalOverlayConnection.addEventListener('click', toggleUpdateTripModal);
-
 function toggleUpdateTripModal() {
   modalUpdateTrip.classList.toggle('active');
 }
+let currentTripCard = null;
 
 // Ajout de l'écouteur d'événements pour le bouton update-trip_button
 document.querySelector('.roadbook_container').addEventListener('click', function(event) {
-  if (event.target.closest('.update-trip_button')) {
+  const button = event.target.closest('.update-trip_button');
+  if (button) {
+    currentTripCard = button.closest('.trip-card_content');
     toggleUpdateTripModal();
   }
 });
+
+// Fonction pour modifier le contenu du innerHTML d'une carte de voyage
+function updateCardContent(card, title, photo, comment, dateStart, dateEnd, duration, note) {
+  // Modifier le contenu de la carte en utilisant les nouvelles données
+  card.querySelector('.trip_title').textContent = title;
+  card.querySelector('.trip_photo').src = photo;
+  card.querySelector('.trip_comment').textContent = `Commentaire : ${comment}`;
+  card.querySelector('.trip_date-start').textContent = `Date de début: ${dateStart}`;
+  card.querySelector('.trip_date-end').textContent = `Date de fin: ${dateEnd}`;
+  card.querySelector('.trip-card_duration').textContent = `Durée du voyage : ${duration} jour(s)`;
+  card.querySelector('.trip_note').textContent = note;
+}
+
+// Fonction pour gérer la soumission du formulaire de modification d'un voyage :
+function handleUpdateFormSubmission(event) {
+  event.preventDefault(); // Empêcher la soumission par défaut du formulaire
+
+  // Récupérer les données du formulaire
+  const updatedTripTitle = document.getElementById('update-trip_title').value;
+  const updatedTripComment = document.getElementById('update-trip_comment').value;
+  const updatedTripDateStart = document.getElementById('update-trip_date-start').value;
+  const updatedTripDateEnd = document.getElementById('update-trip_date-end').value;
+  const updatedTripNote = document.getElementById('update-trip_note').value;
+
+  //Calcul de la durée en fonction des dates renseignées :
+  const updatedStartDate = new Date(updatedTripDateStart);
+  const updatedEndDate = new Date(updatedTripDateEnd);
+  const updatedTripDuration = (Math.ceil((Math.abs(updatedEndDate) - (updatedStartDate))) / (1000 * 60 * 60 * 24));
+  console.log(updatedTripDuration);   
+
+  // Sélectionner l'élément input de type file pour l'image
+  const updatedTripPhotoInput = document.getElementById('update-trip_photo');
+
+  // Vérifier s'il y a un fichier sélectionné
+  if (updatedTripPhotoInput.files.length > 0) {
+    // Créer un objet FileReader
+    const reader = new FileReader();
+    // Écouter lorsque la lecture du fichier est terminée
+    reader.onload = function(event) {
+      // Récupérer l'URL de données (data URL) de l'image
+      const updatedImageUrl = event.target.result;
+      // Modifier le contenu de la carte de voyage avec les nouvelles données
+      updateCardContent(currentTripCard, updatedTripTitle, updatedImageUrl, updatedTripComment, updatedTripDateStart, updatedTripDateEnd, updatedTripDuration, updatedTripNote);
+    };
+    // Lire le contenu du fichier en tant qu'URL de données (data URL)
+    reader.readAsDataURL(updatedTripPhotoInput.files[0]);
+  }else {
+    // Si pas de nouvelle image, utiliser l'image existante
+    const existingImageUrl = currentTripCard.querySelector('.trip_photo').src;
+    updateCardContent(currentTripCard, updatedTripTitle, existingImageUrl, updatedTripComment, updatedTripDateStart, updatedTripDateEnd, updatedTripDuration, updatedTripNote);
+  }
+}
+
+// Sélectionner le formulaire de modification de carte
+const updateTripForm = document.querySelector("#update-trip_form");
+
+updateTripForm.addEventListener("submit", handleUpdateFormSubmission);
+updateTripForm.addEventListener("submit", toggleUpdateTripModal);
+updateTripForm.addEventListener("submit", updateTripForm.reset);
---
+++ js/test-modal-update.js
@@ -0,0 +1,8 @@
+//Fonction pour afficher le modal de modification
+function updatedCard(title, photo, comment, dateStart, dateEnd, duration,note) {
+  const card = document.querySelector('.card');
+  card.classList.add('updated');
+}
+
+
+
